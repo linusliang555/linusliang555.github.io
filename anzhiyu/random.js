1668c2cfd1426ba88892d4c4777a415a403abee4
linusliang555 <linuxliang5@gmail.com>
2024-09-17 18:11:46 +0800

    Site updated: 2024-09-17 18:11:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/16/hello-world/","2024/08/15/最炸裂的一次/"];function toRandomPost(){
+var posts=["2024/09/17/az/","2024/08/15/最炸裂的一次/","1970/01/01/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
